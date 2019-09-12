0eea7417c90d6817e51a085adbf2212a633d67a3
Arman Arutyunov <arman.sharvel@gmail.com>
2019-09-12 22:03:40 +0200

    Initial Commit

---
+++ TronaldDump/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TronaldDump
+//
+//  Created by Arman Arutyunov on 12.09.2019.
+//  Copyright © 2019 Arman Arutyunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
