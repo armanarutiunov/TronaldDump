7eb2ba2388a57c24cd8fe75fa563acac05d1124c
Arman Arutyunov <arman.sharvel@gmail.com>
2019-09-17 13:12:41 +0200

    URLConvertibleTests

---
+++ TronaldDumpTests/Utils/URLConvertibleTests.swift
@@ -0,0 +1,23 @@
+//
+//  URLConvertibleTests.swift
+//  TronaldDumpTests
+//
+//  Created by Arman Arutyunov on 17.09.2019.
+//  Copyright © 2019 Arman Arutyunov. All rights reserved.
+//
+
+import XCTest
+@testable import TronaldDump
+
+class URLConvertibleTests: XCTestCase {
+
+	func testUrl() {
+		let urlString = "https://dice.fm"
+		let url = urlString.url
+		XCTAssertEqual(url?.absoluteString, urlString)
+		XCTAssertEqual(url?.string, urlString)
+		XCTAssertEqual(url?.absoluteString, url?.string)
+		XCTAssertNotNil(url?.forceUnwrappedUrl)
+	}
+
+}
